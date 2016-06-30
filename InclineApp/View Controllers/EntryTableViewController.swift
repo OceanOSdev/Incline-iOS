6c3a58e296a6bc69b43ae0b3b153e0e5afcd18cb
Jake Schroeder <jakeschroeder8@gmail.com>
2016-06-30 19:48:45 -0400

    added fuck nigs

--- InclineApp/View Controllers/EntryTableViewController.swift
+++ InclineApp/View Controllers/EntryTableViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+//fuck nigs
+
 class EntryTableViewController: UITableViewController {
     
  
---
+++ Pods/Target Support Files/ADALiOS-iOS8.4/ADALiOS-iOS8.4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ADALiOS_iOS8_4 : NSObject
+@end
+@implementation PodsDummy_ADALiOS_iOS8_4
+@end
