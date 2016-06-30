9d6dc8d95f482db7a787debfa052ad1d5176d228
thomas maloney <ThomasTNF@live.com>
2016-06-29 22:59:27 -0500

    Fixed Bug with app launch.

--- InclineApp/View Controllers/EntryTableViewController.swift
+++ InclineApp/View Controllers/EntryTableViewController.swift
@@ -25,31 +25,32 @@ class EntryTableViewController: UITableViewController {
         WebApiConnector.getToken(false, parent: self) {
             (result: String?, error: NSError?) -> Void in
             if result != nil {
-                print(result)
+                //print(result)
+                let manager = AFHTTPSessionManager()
+                manager.responseSerializer = AFJSONResponseSerializer()
+                manager.requestSerializer = AFJSONRequestSerializer()
+                manager.requestSerializer.setValue("Bearer \(ApplicationData.graphToken!)", forHTTPHeaderField: "Authorization")
+                //manager.dataTaskWithRequest(NSURLRequest().HTTPM, completionHandler: ((NSURLResponse, AnyObject?, NSError?) -> Void)?)
+                
+                manager.GET("https://graph.windows.net/me?api-version=1.6", parameters: nil, success: { (task: NSURLSessionDataTask, response:AnyObject?) in
+                    //print(response)
+                    let responseDict = response as! Dictionary<String, AnyObject>
+                    let firstName = (responseDict["givenName"] as AnyObject?) as? String
+                    let lastName = (responseDict["surname"] as AnyObject?) as? String
+                    let email = (responseDict["mail"] as AnyObject?) as? String
+                    ApplicationData.Name = "\(firstName!) \(lastName!)"
+                    ApplicationData.Email = email!
+                    //print("\(token!) \(responseDict["surname"]!)")
+                }) { (operation:NSURLSessionDataTask?, error:NSError) in
+                    print(error)
+                }
+
             }
             else {
                 
             }
         }
-        let manager = AFHTTPSessionManager()
-        manager.responseSerializer = AFJSONResponseSerializer()
-        manager.requestSerializer = AFJSONRequestSerializer()
-        manager.requestSerializer.setValue("Bearer \(ApplicationData.graphToken!)", forHTTPHeaderField: "Authorization")
-        //manager.dataTaskWithRequest(NSURLRequest().HTTPM, completionHandler: ((NSURLResponse, AnyObject?, NSError?) -> Void)?)
         
-        manager.GET("https://graph.windows.net/me?api-version=1.6", parameters: nil, success: { (task: NSURLSessionDataTask, response:AnyObject?) in
-            //print(response)
-            let responseDict = response as! Dictionary<String, AnyObject>
-            let firstName = (responseDict["givenName"] as AnyObject?) as? String
-            let lastName = (responseDict["surname"] as AnyObject?) as? String
-            let email = (responseDict["mail"] as AnyObject?) as? String
-            ApplicationData.Name = "\(firstName!) \(lastName!)"
-            ApplicationData.Email = email!
-            //print("\(token!) \(responseDict["surname"]!)")
-        }) { (operation:NSURLSessionDataTask?, error:NSError) in
-            print(error)
-        }
-
 
 
     }
